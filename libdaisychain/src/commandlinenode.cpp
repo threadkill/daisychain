0ebe70341e1ed807ecc8fd82a150dee37638b47e
S P <sjparker@users.noreply.github.com>
2025-02-13 18:41:20 -0800

    fixed string encoding for commandline node on windows.

--- libdaisychain/src/commandlinenode.cpp
+++ libdaisychain/src/commandlinenode.cpp
@@ -221,7 +221,7 @@ CommandLineNode::run_command (const std::string& input, const std::string& sandb
     LDEBUG << LOGNODE << "Executing command line: " << expanded_command_;
 
     std::string std_out;
-    stat = create_process (expanded_command_, std_out);
+    stat = create_process (command_, std_out);
 
     // Log the output from the child process
     if (!std_out.empty()) {
@@ -359,7 +359,7 @@ bool
 CommandLineNode::create_process (const std::string& command, std::string& output)
 {
     std::string pipename = R"(\\.\pipe\)" + name_ + "_" + id_;
-    std::string cmd = "cmd.exe /V:ON /S /C " + command;
+    std::string cmd = "cmd.exe /V:ON /S /C \"call " + command + "\"";
 
     STARTUPINFOEXA si;
     PROCESS_INFORMATION pi;
